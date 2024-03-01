dfc67e3970303c949a496d0fdd3d0b497f96033f
victorgaenshirt <vgaenshirt@posteo.de>
2024-03-02 00:36:38 +0100

    musikvideo und Impressum

--- src/components/sites/Impressum.tsx
+++ src/components/sites/Impressum.tsx
@@ -3,14 +3,20 @@ import content from '../../content.json'
 
 
 export const Impressum: React.FC = () => {
+    const impressumText = content.impressum.impressum.replace(/\n/g, '<br>');
+    const datenschutzText = content.impressum.datenschutz.replace(/\n/g, '<br>');
+
     return (
         <div>
-            <h2 className={"text-lg m-6"}>
-                {content.impressum.impressum}
+            <h2 className={"text-lg font-medium p-6 max-w-4xl mx-auto leading-relaxed"}>
+                <span dangerouslySetInnerHTML={{ __html: impressumText }} />
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <span dangerouslySetInnerHTML={{ __html: datenschutzText }} />
             </h2>
-            <div className={"text-lg m-6"}>
-                {content.impressum.datenschutz}
-            </div>
         </div>
     );
 };
--- src/components/sites/Musikvideo.tsx
+++ src/components/sites/Musikvideo.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-import hz1 from "../../assets/Hochzeit_12.jpg"
-import hz2 from "../../assets/hochzeit.jpg"
-import hz3 from "../../assets/blumenm.jpg"
-import hz4 from "../../assets/Hochzeit_08.jpg"
-import hz5 from "../../assets/Hochzeit_15.jpg"
-import hz6 from "../../assets/Hochzeit_16.1.jpg"
-import hz7 from "../../assets/Hochzeit_14.jpg"
-import hz8 from "../../assets/Hochzeit_17.jpg"
-import hz9 from "../../assets/Hochzeit_19.jpg"
+import mv1 from "../../assets/Musikvideo_01.jpg"
+import mv2 from "../../assets/Musikvideo_02.jpg"
+import mv3 from "../../assets/Musikvideo_03.jpg"
+import mv5 from "../../assets/Musikvideo_05.jpg"
+import mv6 from "../../assets/Musikvideo_06.jpg"
+import mv7 from "../../assets/Musikvideo_07.jpg"
+import mv8 from "../../assets/Musikvideo_08.jpg"
+import mv9 from "../../assets/Musikvideo_09.jpg"
+import mv10 from "../../assets/Musikvideo_10.jpg"
+import mv11 from "../../assets/Musikvideo_11.jpg"
+import mv12 from "../../assets/Musikvideo_12.jpg"
+import mv13 from "../../assets/Musikvideo_13.jpg"
+import mv14 from "../../assets/Musikvideo_14.jpg"
+import mv15 from "../../assets/Musikvideo_15.jpg"
 import cale from "../../assets/Cale.mp4"
 
 export const Musikvideo: React.FC = () => {
@@ -21,10 +26,9 @@ export const Musikvideo: React.FC = () => {
                         <source src={cale} type="video/mp4" />
                     </video>
                     <p className="text-left my-3 text-lg leading-relaxed sm:my-6">
-                        Tauchen Sie ein in die emotionale Welt der Liebe und Romantik, während wir die schönsten
-                        Momente Ihres besonderen Tages einfangen und in zeitlose Erinnerungen verwandeln.
+                        Kreative Musikvideos, die die Botschaft eines Künstlers oder einer Band visuell zum Leben erwecken.
                     </p>
-                    <div className="flex max-w-7xl mx-auto flex-1 items-stretch">
+                    {/*<div className="flex max-w-7xl mx-auto flex-1 items-stretch">
                         <div className="flex-col w-3/5 pr-3">
                             <img className="pb-3" src={hz3} alt="" />
                             <img src={hz2} alt="" />
@@ -32,24 +36,28 @@ export const Musikvideo: React.FC = () => {
                         <div className="w-2/5 flex flex-col justify-between">
                             <img className="flex flex-1" src={hz1} alt="" />
                         </div>
-                    </div>
-                    <p className="text-left my-3 text-lg leading-relaxed sm:my-6">
-                        Mit professioneller Produktion und einem erfahrenen Team erfassen
-                        wir jeden Augenblick, damit Sie ihn immer wieder erleben können.
-                        Vertrauen Sie uns, um Ihre Liebe und Ihren Glanz in bewegenden Bildern festzuhalten und sie zu
-                        zeitlosen Erinnerungen zu machen.
-                    </p>
+                    </div>*/}
                     <div className={"grid grid-cols-2 sm:grid-cols-3 gap-3"}>
-                        <img src={hz4} alt="" />
-                        <img src={hz5} alt="" />
-                        <img src={hz6} alt="" />
-                        <img src={hz7} alt="" />
-                        <img src={hz8} alt="" />
-                        <img src={hz9} alt="" />
+                        <img src={mv1} alt="" />
+                        <img src={mv3} alt="" />
+                        <img src={mv5} alt="" />
+                        <img src={mv7} alt="" />
+                        <img src={mv8} alt="" />
+                        <img src={mv9} alt="" />
+                        <img src={mv10} alt="" />
+                        <img src={mv11} alt="" />
+                        <img src={mv12} alt="" />
+                        <img src={mv13} alt="" />
+                        <img src={mv14} alt="" />
+                        <img src={mv15} alt="" />
+                    </div>
+                    <div className={"grid grid-cols-2 sm:grid-cols-4 gap-3"}>
                     </div>
                     <p className="text-left my-3 text-lg leading-relaxed sm:my-6">
-                        Unsere Hochzeitsvideos sind mehr als nur Aufnahmen - sie sind gefühlvolle Erinnerungen an den
-                        schönsten Tag in Ihrem Leben.
+                        Wir bieten maßgeschneiderte Videokonzepte  für Künstler und Bands, um ihre Musik visuell zum
+                        Leben zu erweckenEin Musikvideo dient an erster Stelle zur Visualisierung und Vermarktung eines
+                        Liedes. Versetzen Sie ihren Zuschauer in eine von Ihnen gewünschte Atmosphäre und erschaffen
+                        Sie zusammen mit Sharkz Media einen einzigartigen Style.
                     </p>
                 </div>
             </div>
