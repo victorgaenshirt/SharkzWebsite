baa4602d120e49e21bd639ba993db9da3afe820b
victorgaenshirt <vgaenshirt@posteo.de>
2024-03-06 01:45:54 +0100

    firebase.js in src

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -16,22 +16,6 @@ import ErrorPage from "../ErrorPage.tsx";
 import Layout from "../Layout.tsx";
 import {SocialMedia} from "../components/sites/SocialMedia.tsx";
 
-import { initializeApp } from "firebase/app";
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDGFc8DhA-QS1Fh0Hp5uK6re6xP6NccZ_8",
-    authDomain: "sharkzwebsite.firebaseapp.com",
-    projectId: "sharkzwebsite",
-    storageBucket: "sharkzwebsite.appspot.com",
-    messagingSenderId: "987195377209",
-    appId: "1:987195377209:web:df1e45c8e585f281c92fe8",
-    measurementId: "G-CF9XT83V3F"
-};
-
-initializeApp(firebaseConfig);
-
-
 const router = createBrowserRouter([
     {
         path: "/",
