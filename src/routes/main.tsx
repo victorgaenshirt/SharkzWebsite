b49b59830b82cdb356cee896e2dfd65e6b638285
victorgaenshirt <vgaenshirt@posteo.de>
2024-03-06 02:23:43 +0100

    try

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -76,6 +76,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+      <Home />
   </React.StrictMode>,
 )
