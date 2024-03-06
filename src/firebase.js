7d181b8da032e4838db024da0bb8001d70f3d799
victorgaenshirt <vgaenshirt@posteo.de>
2024-03-06 01:50:43 +0100

    firebase.js in src

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase-admin/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDGFc8DhA-QS1Fh0Hp5uK6re6xP6NccZ_8",
@@ -14,4 +13,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const firestore = getFirestore();
