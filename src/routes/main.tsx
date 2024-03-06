2fb84e2819dc193fa5a362cc36c5e41dba6888e3
victorgaenshirt <vgaenshirt@posteo.de>
2024-03-06 01:25:04 +0100

    main.tsx

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -17,8 +17,7 @@ import Layout from "../Layout.tsx";
 import {SocialMedia} from "../components/sites/SocialMedia.tsx";
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase-admin/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDGFc8DhA-QS1Fh0Hp5uK6re6xP6NccZ_8",
@@ -31,8 +30,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore();
 
 
 const router = createBrowserRouter([
